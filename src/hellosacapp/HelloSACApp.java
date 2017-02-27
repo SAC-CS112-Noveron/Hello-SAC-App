be21b9c933a51d27e94d169eeabaa644efd79764
Noveron <tui_games_02@yahoo.com>
2017-02-26 20:19:02 -0800

    Hello SAC
    Javier Noveron

---
+++ src/hellosacapp/HelloSACApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellosacapp;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello SAC!" to standard output.
+ */
+public class HelloSACApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello Santa Ana College !"); // Display the string.
+    }
+    
+}
